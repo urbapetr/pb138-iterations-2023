f214b35cc955bf5fc834ead4bba05d70cd76ba0d
Petr Urbanek <xurbane3@fi.muni.cz>
2023-05-24 16:04:21 +0200

    feat: done Auction, Item, ShipCard and PlayerCard. Looks good to me

--- iteration-07/src/stories/Item/Item.tsx
+++ iteration-07/src/stories/Item/Item.tsx
@@ -1,10 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './item.css';
 
-export function Item() {
-    return (
-        <></>
-    )
+interface ItemProps {
+  itemName: string;
+  itemClassification: string;
+  itemPrice: number;
+  itemDescription: string;
+  itemImage: string;
+}
+
+export function Item({ itemName, itemClassification, itemPrice, itemDescription, itemImage }: ItemProps) {
+  const [descriptionVisible, setDescriptionVisible] = useState(false);
+
+  const toggleDescription = () => {
+    setDescriptionVisible(!descriptionVisible);
+  };
+
+  return (
+    <div className="item" onClick={toggleDescription}>
+      <img src={itemImage} alt="" className="background-image" />
+      <div className="item__preview">
+        <div className="item__name">
+          <h2>{itemName}</h2>
+          <h3>{itemClassification}</h3>
+        </div>
+        <p className="item__price">{itemPrice}</p>
+      </div>
+      {descriptionVisible && (
+        <div className="item__description item__description--enabled">
+          <p className="item__description-text">{itemDescription}</p>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default Item;
--- iteration-07/src/stories/ShipCard/ShipCard.tsx
+++ iteration-07/src/stories/ShipCard/ShipCard.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
-import "./shipCard.css";
+import './shipCard.css';
 
-export function ShipCard() {
-    return (
-        <></>
-    )
+export interface ShipCardProps {
+  shipName: string;
+  shipImage: string;
+}
+
+export function ShipCard({ shipName, shipImage }: ShipCardProps) {
+  return (
+    <div className="ship-card ship-card--selected">
+      <img src={shipImage} alt="Ship" className="background-image" />
+      <div className="ship-card__content">
+        <h2 className="ship-card__title">{shipName}</h2>
+        <div className="ship-card__selection-icon-wrapper">
+          <img src="../assets/selected.svg" alt="Selected icon" className="ship-card__selection-icon" />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default ShipCard;
